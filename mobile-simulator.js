6feac50c1946d4fb06140aac51f096a270359062
abandingridwedding-maker <abandingridwedding@gmail.com>
2026-05-27 14:53:18 +0200

    fix topo again 2

--- mobile-simulator.js
+++ mobile-simulator.js
@@ -461,27 +461,11 @@ function getSmartZoom() {
 }
 
 function getOverviewRect() {
-  const padX = 2;
-  const padY = 2;
-
-  const screenW = mobilePitch.width;
-  const screenH = mobilePitch.height;
-
-  const fieldRatio = 1200 / 700;
-
-  let w = screenW - padX * 2;
-  let h = w / fieldRatio;
-
-  if (h > screenH - padY * 2) {
-    h = screenH - padY * 2;
-    w = h * fieldRatio;
-  }
-
   return {
-    x: (screenW - w) / 2,
-    y: (screenH - h) / 2,
-    w,
-    h
+    x: -60,
+    y: 6,
+    w: mobilePitch.width + 120,
+    h: mobilePitch.height - 12
   };
 }
 
